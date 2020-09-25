e4bd28d9de4009db3375d05c470354df8b91b162
Skipper Seabold <jsseabold@gmail.com>
2020-09-25 11:57:02 -0500

    Follow redirects

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,6 @@
 # C.vim
 
 cd ~/.vim
-curl http://www.vim.org/scripts/download_script.php?src_id=9679 -o cvim.zip
+curl -L http://www.vim.org/scripts/download_script.php?src_id=9679 -o cvim.zip
 unzip cvim.zip
 rm cvim.zip
